aae381fa226ffd1148e0796e4bc6634db15d35ba
Nicolas Moutschen <nicolas.moutschen@gmail.com>
2019-12-29 18:58:14 +0100

    Add test cases for node.go

--- gossip/node.go
+++ gossip/node.go
@@ -44,6 +44,32 @@ func NewNode(ip string, port int) *Node {
 	return n
 }
 
+//AddPeer adds a new peer if there are no known peers with the same Config
+func (n *Node) AddPeer(config Config) {
+	log.WithFields(log.Fields{"node": n, "config": config, "func": "addPeerWorker"}).Info("Received peering request")
+
+	//Skip if self.
+	if config == n.Config {
+		log.WithFields(log.Fields{"node": n, "config": config, "func": "addPeerWorker"}).Info("Skip self-peering request")
+		return
+	}
+
+	//Skip if already known.
+	if _, found := n.FindPeer(config); found {
+		log.WithFields(log.Fields{"node": n, "config": config, "func": "addPeerWorker"}).Info("Skip known peer")
+		return
+	}
+
+	//Add the peer to the list of known peers.
+	peer := &Peer{
+		Config: config,
+	}
+	n.Peers = append(n.Peers, peer)
+
+	//Send a peering request.
+	go peer.SendPeeringRequest(n.Config)
+}
+
 /*FindPeer looks up known peers and returns if there is a peer matching the
 Config provided.
 */
@@ -56,6 +82,81 @@ func (n *Node) FindPeer(config Config) (int, bool) {
 	return -1, false
 }
 
+/*PeerSendState sends a state to peers.
+
+If the number of peers known to this node is greater than PeerMaxRecipients,
+this function takes PeerMaxRecipients peers at random and sends the state to
+only those peers.
+*/
+func (n *Node) PeerSendState(state State) int {
+	var peers []*Peer
+	//If there are too many peers, need to limit to PeerMaxRecipients peers
+	//chosen randomly.
+	if len(n.Peers) > PeerMaxRecipients {
+		for _, i := range rand.Perm(len(n.Peers)) {
+			peers = append(peers, n.Peers[i])
+			if len(peers) >= PeerMaxRecipients {
+				break
+			}
+		}
+	} else {
+		peers = n.Peers
+	}
+	log.WithFields(log.Fields{"node": n, "state": state, "func": "peerSendStateWorker"}).Infof("Sending state update to %d/%d peers", len(peers), len(n.Peers))
+
+	for _, peer := range peers {
+		go peer.Send(state)
+	}
+
+	return len(peers)
+}
+
+//PingPeers ping all peers known to the node
+func (n *Node) PingPeers() {
+	var peersToRemove []int
+	for i, peer := range n.Peers {
+		/*If the peer is irrecoverable, mark it for removal from the list
+		of known peers.
+		*/
+		if peer.IsIrrecoverable() {
+			peersToRemove = append(peersToRemove, i)
+			continue
+		}
+
+		/*Despite running a goroutine in a loop, we don't need to wait for
+		completion here as this routine doesn't have any side effect to the
+		pingWorker.
+
+		peersToRemove is filled before this routine, therefore it is
+		already ready to be processed, without having to wait for the
+		goroutines to finish their execution.
+		*/
+		go func(n *Node, peer *Peer) {
+			peer.Ping()
+			if peer.LastState > n.State.Timestamp {
+				n.fetchStateChan <- peer
+			}
+		}(n, peer)
+	}
+
+	/* Every time we remove a peer 'i' from n.Peers, the new index of peers
+	greater than i is reduced by 1. If peersToRemove is not sorted, an item
+	in the middle could be remove, leaving items on the left and right in
+	to be removed. If that's the case, it will be hard to know if we need
+	to decrease the index by the number of items remove.
+
+	By sorting the slice, we ensure consistent behavior (always needing to
+	substract the position by the number of items removed).
+	*/
+	sort.Ints(peersToRemove)
+
+	//Process irrecoverable peers.
+	for c, i := range peersToRemove {
+		log.WithFields(log.Fields{"node": n, "func": "PingPeers", "peer": n.Peers[i]}).Info("Removing irrecoverable peer")
+		n.Peers = append(n.Peers[:i-c], n.Peers[i-c+1:]...)
+	}
+}
+
 //Run start the workers and run an HTTP server
 func (n *Node) Run() {
 	//Start workers
@@ -63,7 +164,7 @@ func (n *Node) Run() {
 	go n.fetchStateWorker()
 	go n.peerSendStateWorker()
 	go n.pingWorker()
-	go n.stateChanWorker()
+	go n.stateWorker()
 
 	//Register handlers
 	http.HandleFunc("/", n.rootHandler)
@@ -81,7 +182,8 @@ func (n Node) String() string {
 }
 
 /*fetchStateWorker waits for peers on the n.fetchStateChan channel and
-retrieves the last state from other peers
+retrieves the last state from those peers, then sends the state to the
+n.stateChan channel.
 */
 func (n *Node) fetchStateWorker() {
 	for {
@@ -120,28 +222,7 @@ network.
 func (n *Node) addPeerWorker() {
 	for {
 		config := <-n.addPeerChan
-		log.WithFields(log.Fields{"node": n, "config": config, "func": "addPeerWorker"}).Info("Received peering request")
-
-		//Skip if self.
-		if config == n.Config {
-			log.WithFields(log.Fields{"node": n, "config": config, "func": "addPeerWorker"}).Info("Skip self-peering request")
-			continue
-		}
-
-		//Skip if already known.
-		if _, found := n.FindPeer(config); found {
-			log.WithFields(log.Fields{"node": n, "config": config, "func": "addPeerWorker"}).Info("Skip known peer")
-			continue
-		}
-
-		//Add the peer to the list of known peers.
-		peer := &Peer{
-			Config: config,
-		}
-		n.Peers = append(n.Peers, peer)
-
-		//Send a peering request.
-		go peer.SendPeeringRequest(n.Config)
+		n.AddPeer(config)
 	}
 }
 
@@ -151,25 +232,7 @@ sends the state to all known peers.
 func (n *Node) peerSendStateWorker() {
 	for {
 		state := <-n.peerStateChan
-
-		var peers []*Peer
-		//If there are too many peers, need to limit to PeerMaxRecipients peers
-		//chosen randomly.
-		if len(n.Peers) > PeerMaxRecipients {
-			for _, i := range rand.Perm(len(n.Peers)) {
-				peers = append(peers, n.Peers[i])
-				if len(peers) >= PeerMaxRecipients {
-					break
-				}
-			}
-		} else {
-			peers = n.Peers
-		}
-		log.WithFields(log.Fields{"node": n, "state": state, "func": "peerSendStateWorker"}).Infof("Sending state update to %d/%d peers", len(peers), len(n.Peers))
-
-		for _, peer := range peers {
-			go peer.Send(state)
-		}
+		n.PeerSendState(state)
 	}
 }
 
@@ -177,56 +240,14 @@ func (n *Node) peerSendStateWorker() {
 func (n *Node) pingWorker() {
 	for {
 		time.Sleep(PingDelay)
-
-		var peersToRemove []int
-		for i, peer := range n.Peers {
-			/*If the peer is irrecoverable, mark it for removal from the list
-			of known peers.
-			*/
-			if peer.IsIrrecoverable() {
-				peersToRemove = append(peersToRemove, i)
-				continue
-			}
-
-			/*Despite running a goroutine in a loop, we don't need to wait for
-			completion here as this routine doesn't have any side effect to the
-			pingWorker.
-
-			peersToRemove is filled before this routine, therefore it is
-			already ready to be processed, without having to wait for the
-			goroutines to finish their execution.
-			*/
-			go func(n *Node, peer *Peer) {
-				peer.Ping()
-				if peer.LastState > n.State.Timestamp {
-					n.fetchStateChan <- peer
-				}
-			}(n, peer)
-		}
-
-		/* Every time we remove a peer 'i' from n.Peers, the new index of peers
-		greater than i is reduced by 1. If peersToRemove is not sorted, an item
-		in the middle could be remove, leaving items on the left and right in
-		to be removed. If that's the case, it will be hard to know if we need
-		to decrease the index by the number of items remove.
-
-		By sorting the slice, we ensure consistent behavior (always needing to
-		substract the position by the number of items removed).
-		*/
-		sort.Ints(peersToRemove)
-
-		//Process irrecoverable peers.
-		for c, i := range peersToRemove {
-			log.WithFields(log.Fields{"node": n, "func": "pingWorker", "peer": n.Peers[i]}).Info("Removing irrecoverable peer")
-			n.Peers = append(n.Peers[:i-c], n.Peers[i-c+1:]...)
-		}
+		n.PingPeers()
 	}
 }
 
-/*stateChanWorker waits for new states on the n.stateChan channel and process
+/*stateWorker waits for new states on the n.stateChan channel and process
 them.
 */
-func (n *Node) stateChanWorker() {
+func (n *Node) stateWorker() {
 	for {
 		state := <-n.stateChan
 
@@ -237,11 +258,11 @@ func (n *Node) stateChanWorker() {
 
 		switch {
 		case state.Timestamp < n.State.Timestamp:
-			log.WithFields(log.Fields{"node": n, "state": state, "func": "stateChanWorker"}).Info("Received obsolete state")
+			log.WithFields(log.Fields{"node": n, "state": state, "func": "stateWorker"}).Info("Received obsolete state")
 		case state.Timestamp == n.State.Timestamp:
-			log.WithFields(log.Fields{"node": n, "state": state, "func": "stateChanWorker"}).Info("Received known state")
+			log.WithFields(log.Fields{"node": n, "state": state, "func": "stateWorker"}).Info("Received known state")
 		case state.Timestamp > n.State.Timestamp:
-			log.WithFields(log.Fields{"node": n, "state": state, "func": "stateChanWorker"}).Info("Received new state")
+			log.WithFields(log.Fields{"node": n, "state": state, "func": "stateWorker"}).Info("Received new state")
 			n.State = state
 			n.peerStateChan <- state
 		}
---
+++ gossip/node_test.go
@@ -0,0 +1,214 @@
+package gossip
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+func TestNewNode(t *testing.T) {
+	config := Config{"127.0.0.1", 8080}
+	n := NewNode(config.IP, config.Port)
+
+	if n.Config != config {
+		t.Errorf("n.Config == %v; want %v", n.Config, config)
+	}
+}
+
+func TestNodeAddPeer(t *testing.T) {
+	n := NewNode("127.0.0.1", 8080)
+	config := Config{"127.0.0.1", 8081}
+
+	// Test if the node skips itself
+	n.AddPeer(Config{"127.0.0.1", 8080})
+
+	if len(n.Peers) != 0 {
+		t.Errorf("len(n.Peers) == %d; want %d", len(n.Peers), 0)
+	}
+
+	//Adding one peer config
+	n.AddPeer(config)
+
+	//If all goes well, there should be one peer in n.Peers
+	if len(n.Peers) != 1 {
+		t.Errorf("len(n.Peers) == %d; want %d", len(n.Peers), 1)
+	}
+	if n.Peers[0].Config != config {
+		t.Errorf("n.Peers[0].Config == %v; want %v", n.Peers[0].Config, config)
+	}
+
+	//Adding the same peer config
+	n.AddPeer(config)
+
+	//Since we are sending the same config, only one peer should be there
+	if len(n.Peers) != 1 {
+		t.Errorf("len(n.Peers) == %d; want %d", len(n.Peers), 1)
+	}
+	if n.Peers[0].Config != config {
+		t.Errorf("n.Peers[0].Config == %v; want %v", n.Peers[0].Config, config)
+	}
+}
+
+func TestNodeFindPeer(t *testing.T) {
+	n := NewNode("127.0.0.1", 8080)
+
+	peer1 := NewPeer(Config{"127.0.0.1", 8081})
+	peer2 := NewPeer(Config{"127.0.0.1", 8082})
+	peer3 := NewPeer(Config{"127.0.0.1", 8083})
+	peer4 := NewPeer(Config{"127.0.0.1", 8084})
+
+	testCases := []struct {
+		Peers  []*Peer
+		Config Config
+		Pos    int
+		Found  bool
+	}{
+		{[]*Peer{}, peer1.Config, -1, false},
+		{[]*Peer{peer1}, peer1.Config, 0, true},
+		{[]*Peer{peer1}, peer2.Config, -1, false},
+		{[]*Peer{peer1, peer2, peer3}, peer1.Config, 0, true},
+		{[]*Peer{peer1, peer2, peer3}, peer2.Config, 1, true},
+		{[]*Peer{peer1, peer2, peer3}, peer3.Config, 2, true},
+		{[]*Peer{peer1, peer2, peer3}, peer4.Config, -1, false},
+	}
+
+	for i, testCase := range testCases {
+		n.Peers = testCase.Peers
+		pos, found := n.FindPeer(testCase.Config)
+		if pos != testCase.Pos {
+			t.Errorf("pos == %d for test case %d; want %d", pos, i, testCase.Pos)
+		}
+		if found != testCase.Found {
+			t.Errorf("found == %t for test case %d; want %t", found, i, testCase.Found)
+		}
+	}
+}
+
+func TestNodeFetchStateWorker(t *testing.T) {
+	var received bool
+	state := State{time.Now().UnixNano(), "Test data"}
+	testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != "GET" {
+			t.Errorf("r.Method == %s; want %s", r.Method, "GET")
+		}
+		if r.URL.Path != "/" {
+			t.Errorf("r.URL.PATH == %s; want %s", r.URL.Path, "/")
+		}
+		received = true
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(state)
+	}))
+	defer func() { testServer.Close() }()
+
+	peer := NewPeer(parseURL(testServer.URL))
+	peer.LastState = state.Timestamp
+
+	n := NewNode("127.0.0.1", 8080)
+	n.Peers = append(n.Peers, peer)
+
+	go n.fetchStateWorker()
+	n.fetchStateChan <- peer
+	newState := <-n.stateChan
+
+	if newState != state {
+		t.Errorf("newState == %v; want %v", newState, state)
+	}
+
+	if !received {
+		t.Errorf("HTTP Server never received a request")
+	}
+}
+
+func TestNodePeerSendStateWorker(t *testing.T) {
+	testCases := []struct {
+		Recipients int
+		Expected   int
+	}{
+		{0, 0},
+		{1, 1},
+		{PeerMaxRecipients, PeerMaxRecipients},
+		{PeerMaxRecipients + 1, PeerMaxRecipients},
+	}
+
+	state := State{time.Now().UnixNano(), "Test data"}
+	n := NewNode("127.0.0.1", 8080)
+
+	for i, testCase := range testCases {
+		for i := 0; i < testCase.Recipients; i++ {
+			n.Peers = append(n.Peers, NewPeer(Config{"127.0.0.1", 8081}))
+		}
+
+		count := n.PeerSendState(state)
+
+		if count != testCase.Expected {
+			t.Errorf("count == %d in test case %d; want %d", count, i, testCase.Expected)
+		}
+	}
+
+	//TODO: Test that testCase.Expected requests are sent to the peers
+}
+
+func TestNodePingPeers(t *testing.T) {
+	//Initialize peer server
+	var received bool
+	peer := &Peer{}
+	peer.UpdateStatus(true)
+	peer.UpdateStatus(false)
+	peer.LastState = time.Now().UnixNano()
+	testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != "GET" {
+			t.Errorf("r.Method == %s; want %s", r.Method, "GET")
+		}
+		if r.URL.Path != "/status" {
+			t.Errorf("r.URL.PATH == %s; want %s", r.URL.Path, "/status")
+		}
+		received = true
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(StatusResponse{
+			LastState: peer.LastState,
+		})
+	}))
+	defer func() { testServer.Close() }()
+	peer.Config = parseURL(testServer.URL)
+
+	//Initialize node
+	n := NewNode("127.0.0.1", 8080)
+	n.Peers = append(n.Peers, peer)
+
+	//Ping all peers
+	go n.PingPeers()
+
+	//Wait for a peer from n.PingPeers()
+	rPeer := <-n.fetchStateChan
+
+	//Check results
+	if rPeer != peer {
+		t.Errorf("rPeer == %v; want %v", rPeer, peer)
+	}
+
+	if peer.Attempts != 0 {
+		t.Errorf("peer.Attempts == %d; want %d", peer.Attempts, 0)
+	}
+
+	if !received {
+		t.Errorf("HTTP Server never received a request")
+	}
+
+	//TODO: test with an unreachable peer
+}
+
+func TestNodeStateWorker(t *testing.T) {
+	state := State{time.Now().UnixNano(), "Test data"}
+	n := NewNode("127.0.0.1", 8080)
+
+	go n.stateWorker()
+	n.stateChan <- state
+
+	_ = <-n.peerStateChan
+
+	if n.State != state {
+		t.Errorf("n.State == %v; want %v", n.State, state)
+	}
+}
