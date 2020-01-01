1c36aaa0b6aada65e68a4d66f4f536f2bdf2851b
Nicolas Moutschen <nicolas.moutschen@gmail.com>
2020-01-01 14:41:49 +0100

    Switch dashboard to cytoscape

--- dashboard/graph.js
+++ dashboard/graph.js
@@ -1,45 +1,48 @@
-var data = {"nodes":[]};
+var colors = {
+    "main": "#5F84F4",
+    "accent": "#314275",
+    "text": "#FFFFFF"
+};
 
 function toString(config) {
     return config["ip"] + ":" + config["port"].toString();
 }
 
 function fetchData(url, callback) {
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.open("GET", url+"/peers", null);
-    xmlhttp.onreadystatechange = function() {
-        if (xmlhttp.readyState == 4) {
-            if(xmlhttp.status == 200) {
-                console.log(xmlhttp.responseText);
-                data = JSON.parse(xmlhttp.responseText);
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", url+"/peers", null);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4) {
+            if(xhr.status == 200) {
+                console.log(xhr.responseText);
+                data = JSON.parse(xhr.responseText);
                 callback(data);
             }
         }
     }
-    xmlhttp.send();
+    xhr.send();
 }
 
 function parseData(data) {
     var nodes = [];
-    var edges = [];
     var analyzed = [];
 
     if (data["nodes"] !== null) {
         data["nodes"].forEach(function(node, i, _) {
             config = toString(node["config"]);
 
-            nodes.push({
-                id: config,
-                label: config
-            });
+            nodes.push({data: {
+                id: config
+            }});
 
             if (node["peers"] !== null) {
                 node["peers"].forEach(function(peer, j, _) {
                     if (!analyzed.includes(toString(peer))) {
-                        edges.push({
-                            from: config,
-                            to: toString(peer)
-                        });
+                        nodes.push({data:{
+                            id: config+"-"+toString(peer),
+                            source: config,
+                            target: toString(peer)
+                        }});
                     }
                 });
             }
@@ -48,36 +51,39 @@ function parseData(data) {
         });
     }
 
-    return {
-        nodes: new vis.DataSet(nodes),
-        edges: new vis.DataSet(edges)
-    };
+    return nodes;
 }
 
-function createGraph(data) {
-    var container = document.getElementById("graph");
-    var options = {
-        nodes: {
-            color: {
-                border: "#0C9B3C",
-                background: "#52CE7B",
-                highlight: {
-                    background: "#0C9B3C"
+window.onload = function() {
+    fetchData(document.getElementById("ctrlAddr").value, function(data) {
+        data = parseData(data);
+        var cy = cytoscape({
+            container: document.getElementById('graph'),
+            elements: data,
+            style: [
+                {
+                    selector: 'node',
+                    style: {
+                        'background-color': colors.main,
+                        'label': 'data(id)',
+                        'font-family': 'Montserrat'
+                    }
+                },
+                {
+                    selector: 'edge',
+                    style: {
+                        'width': 2,
+                        'line-color': colors.accent
+                    }
                 }
+            ],
+            layout: {
+                name: "circle"
             }
-        },
-        edges: {
-            color: {
-                color: "#0C9B3C"
-            }
-        }
-    };
-    var network = new vis.Network(container, data, options);
-}
-
-document.getElementById("ctrlUpdate").onclick = function() {
-    var url = document.getElementById("ctrlAddr").value;
-    fetchData(url, function(data) {
-        createGraph(parseData(data));
+          });
     });
-}
+
+    document.getElementById("ctrlUpdate").onclick = function() {
+
+    }
+};
