c015dee5488b57835034e29d65482b6d285c40f2
Nicolas Moutschen <nicolas.moutschen@gmail.com>
2020-01-02 11:03:46 +0100

    Add yaml hints

--- gossip/config.go
+++ gossip/config.go
@@ -7,59 +7,59 @@ import (
 //ControllerConfig represents the configuration properties for controllers
 type ControllerConfig struct {
 	//MinPeers is the minimum number of peers that any peer should have
-	MinPeers int `json:"minPeers"`
+	MinPeers int `json:"minPeers" yaml:"minPeers"`
 	/*MaxPingDelay is the time (in ms) before the controller will consider a
 	peer as irrecoverable*/
-	MaxPingDelay int64 `json:"maxPingDelay"`
+	MaxPingDelay int64 `json:"maxPingDelay" yaml:"maxPingDelay"`
 	/*ScanInterval is the delay (in ms) between two scans from a controller
 	instance*/
-	ScanInterval time.Duration `json:"scanInterval"`
+	ScanInterval time.Duration `json:"scanInterval" yaml:"scanInterval"`
 }
 
 //CorsConfig represents the configuration properties for CORS
 type CorsConfig struct {
 	/*AllowHeaders is used for the Access-Control-Allow-Headers header for HTTP
 	responses.*/
-	AllowHeaders string `json:"allowHeaders"`
+	AllowHeaders string `json:"allowHeaders" yaml:"allowHeaders"`
 	/*AllowOrigin is used for the Access-Control-Allow-Origin header for HTTP
 	responses.*/
-	AllowOrigin string `json:"allowOrigin"`
+	AllowOrigin string `json:"allowOrigin" yaml:"allowOrigin"`
 }
 
 //NodeConfig represents the configuration properties for nodes
 type NodeConfig struct {
 	/*MaxRecipients is the maximum number of peers to a node that could receive
 	a message*/
-	MaxRecipients int `json:"maxRecipients"`
+	MaxRecipients int `json:"maxRecipients" yaml:"maxRecipients"`
 	/*MaxPingDelay is the time (in ms) before the node will consider a
 	peer as irrecoverable*/
-	MaxPingDelay int64 `json:"maxPingDelay"`
+	MaxPingDelay int64 `json:"maxPingDelay" yaml:"maxPingDelay"`
 	//ScanInterval is the delay (in ms) between two pings from a node instance
-	PingInterval time.Duration `json:"pingInterval"`
+	PingInterval time.Duration `json:"pingInterval" yaml:"pingInterval"`
 }
 
 //PeerConfig represents the configuration properties for peers
 type PeerConfig struct {
 	/*BackoffDuration is the base duration (in ms) before retrying to send a
 	message to a peer*/
-	BackoffDuration time.Duration `json:"backoffDuration"`
+	BackoffDuration time.Duration `json:"backoffDuration" yaml:"backoffDuration"`
 	/*MaxAttempts is the number of attempts before considering the peer as
 	unreachable*/
-	MaxAttempts int `json:"maxAttempts"`
+	MaxAttempts int `json:"maxAttempts" yaml:"maxAttempts"`
 	/*MaxRetries is the number of retries before giving up on sending a message
 	to a peer*/
-	MaxRetries int `json:"maxRetries"`
+	MaxRetries int `json:"maxRetries" yaml:"maxRetries"`
 }
 
 //Config represents all configuration properties
 type Config struct {
-	Controller ControllerConfig `json:"controller"`
-	Cors       CorsConfig       `json:"cors"`
-	Node       NodeConfig       `json:"node"`
-	Peer       PeerConfig       `json:"peer"`
+	Controller ControllerConfig `json:"controller" yaml:"controller"`
+	Cors       CorsConfig       `json:"cors" yaml:"cors"`
+	Node       NodeConfig       `json:"node" yaml:"node"`
+	Peer       PeerConfig       `json:"peer" yaml:"peer"`
 	/*Protocol is the protocol to use to send messages to other peers, either
 	http or https*/
-	Protocol string `json:"protocol"`
+	Protocol string `json:"protocol" yaml:"protocol"`
 }
 
 //DefaultConfig is the default configuration for controllers, nodes and peers.
